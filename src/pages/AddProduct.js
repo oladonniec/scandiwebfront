f2a8888e2ec076bb91f0288c3c0c42eda9851059
Lukman Olawale Oladosu <oladosul@accessbankplc.com>
2023-02-13 22:45:57 +0100

    updates

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -4,42 +4,130 @@ import { useNavigate } from "react-router-dom";
 
 const AddProduct = () => {
   const navigate = useNavigate();
-  const [currentProduct, setCurrentProduct] = useState("typeswitcher");
 
-  const [sku, setSku] = useState("");
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [size, setSize] = useState("");
-  const [weight, setWeight] = useState("");
-  const [dimensions, setDimensions] = useState();
-  const [height, setHeight] = useState("");
-  const [width, setWidth] = useState("");
-  const [length, setLength] = useState("");
+  const [productForm, setProductForm] = useState({
+    product_type: "",
+    sku: "",
+    name: "",
+    price: "",
+    size: "",
+    weight: "",
+    height: "",
+    width: "",
+    length: "",
+  });
 
-  const handletypeChange = (event) => {
-    setCurrentProduct(event.target.value);
+  const [errors, setErrors] = useState({
+    product_type: "",
+    sku: "",
+    name: "",
+    price: "",
+    size: "",
+    weight: "",
+    height: "",
+    width: "",
+    length: "",
+  });
+
+  const handleChange = (event) => {
+    setProductForm({
+      ...productForm,
+      [event.target.name]: event.target.value,
+    });
+
+    setErrors({
+      ...errors,
+      [event.target.name]: event.target.value ? "" : "This field is required",
+    });
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    let formData = new FormData();
-    formData.append("sku", sku);
-    formData.append("name", name);
-    formData.append("price", price);
-    if (currentProduct === "discs") {
-      formData.append("size", size);
-    } else if (currentProduct === "books") {
-      formData.append("weight", weight);
-    } else if (currentProduct === "furnitures") {
-      setDimensions(height + "x" + width + "x" + length);
-      console.log(dimensions);
-      formData.append("dimension", dimensions);
+    let isFormValid = true;
+
+    setErrors({});
+
+    if (!productForm.sku) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        sku: "SKU is required",
+      }));
+      isFormValid = false;
     }
-    const url = "http://localhost:8080/scandiback/api/posts/insert.php";
-    axios
-      .post(url, formData)
-      .then((res) => console.log(res.data))
+
+    if (!productForm.name) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        name: "Name is required",
+      }));
+      isFormValid = false;
+    }
+
+    if (!productForm.price) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        price: "Price is required",
+      }));
+      isFormValid = false;
+    }
+
+    if (!productForm.product_type) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        product_type: "Product Type is required",
+      }));
+      isFormValid = false;
+    }
+
+    if (productForm.product_type === "disc") {
+      if (!productForm.size) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          size: "Size is required",
+        }));
+        isFormValid = false;
+      }
+    }
+
+    if (productForm.product_type === "book") {
+      if (!productForm.weight) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          weight: "Weight is required",
+        }));
+        isFormValid = false;
+      }
+    }
+
+    if (productForm.product_type === "furniture") {
+      if (!productForm.height || !productForm.width || !productForm.length) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          height: "Height is required",
+          width: "Width is required",
+          length: "Length is required",
+        }));
+        isFormValid = false;
+      }
+    }
+
+    if (!isFormValid) {
+      console.log("Its error");
+      return;
+    }
+
+    const formDataToSubmit = new FormData();
+
+    for (const key in productForm) {
+      formDataToSubmit.append(key, productForm[key]);
+    }
+
+    const url =
+      "https://scandiwebphpapi.000webhostapp.com/scandiback/api/addProducts.php";
+    await axios
+      .post(url, formDataToSubmit)
+      .then((res) => console.log(res))
       .catch((err) => console.log(err));
 
     navigate("/");
@@ -47,7 +135,7 @@ const AddProduct = () => {
 
   return (
     <div className="productAddContainer">
-      <div className="row">
+      <div className="row between">
         <h2>Product Add</h2>
         <div>
           <button type="submit" id="save-button" onClick={handleSubmit}>
@@ -58,60 +146,73 @@ const AddProduct = () => {
       </div>
       <hr></hr>
       <form action="" id="product_form">
+        <React.Fragment>
+          <div>
+            <label htmlFor="sku">SKU</label>
+            <input
+              name="sku"
+              type="text"
+              id="sku"
+              required
+              placeholder="SKU"
+              value={productForm.sku}
+              onChange={handleChange}
+            />
+          </div>
+          {errors.sku && <p>{errors.sku}</p>}
+        </React.Fragment>
+        <React.Fragment>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              name="name"
+              type="text"
+              id="name"
+              required
+              placeholder="Name"
+              value={productForm.name}
+              onChange={handleChange}
+            />
+          </div>
+          {errors.name && <p>{errors.name}</p>}
+        </React.Fragment>
+        <React.Fragment>
+          <div>
+            <label htmlFor="price">Price</label>
+            <input
+              name="price"
+              type="text"
+              id="price"
+              required
+              placeholder="Price"
+              value={productForm.price}
+              onChange={handleChange}
+            />
+          </div>
+          {errors.price && <p>{errors.price}</p>}
+        </React.Fragment>
         <div>
-          <label htmlFor="sku">SKU</label>
-          <input
-            name="sku"
-            type="text"
-            id="sku"
-            placeholder="SKU"
-            value={sku}
-            onChange={(e) => setSku(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            name="name"
-            type="text"
-            id="name"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="price">Price</label>
-          <input
-            name="price"
-            type="text"
-            id="price"
-            placeholder="Price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="type-switcher">Type Switcher</label>
+          <label htmlFor="product_type">Type Switcher</label>
           <select
-            onChange={handletypeChange}
-            defaultValue={currentProduct}
-            id="productType"
+            onChange={handleChange}
+            defaultValue={productForm.product_type}
+            id="product_type"
+            name="product_type"
           >
-            <option value="typeswitcher">Type Switcher</option>
-            <option value="discs" id="DVD">
+            <option value="">Type Switcher</option>
+            <option value="disc" id="DVD">
               DVD
             </option>
-            <option value="furnitures" id="Furniture">
+            <option value="furniture" id="Furniture">
               Furnitures
             </option>
-            <option value="books" id="Book">
+            <option value="book" id="Book">
               Books
             </option>
           </select>
         </div>
         <div>
-          {currentProduct === "books" ? (
+          {productForm.product_type === "book" ? (
             <div className="productSwitch">
               <h3>Books</h3>
               <React.Fragment>
@@ -119,16 +220,19 @@ const AddProduct = () => {
                 <input
                   name="weight"
                   type="text"
+                  value={productForm.weight}
                   id="weight"
+                  required
                   placeholder="Weight"
-                  onChange={(e) => setWeight(e.target.value)}
+                  onChange={handleChange}
                 />
+                {errors.weight && <h3>{errors.weight}</h3>}
               </React.Fragment>
               <p>
-                <small>This is the books description</small>
+                <small>Please Provide Weight</small>
               </p>
             </div>
-          ) : currentProduct === "discs" ? (
+          ) : productForm.product_type === "disc" ? (
             <div className="productSwitch">
               <h3>DVD</h3>
               <React.Fragment>
@@ -137,49 +241,61 @@ const AddProduct = () => {
                   name="size"
                   type="text"
                   id="size"
+                  value={productForm.size}
                   placeholder="Size"
-                  onChange={(e) => setSize(e.target.value)}
+                  required
+                  onChange={handleChange}
                 />
+                {errors.size && <p>{errors.size}</p>}
               </React.Fragment>
               <p>
-                <small>This is the dvd description</small>
+                <small>Please Provide Size</small>
               </p>
             </div>
-          ) : currentProduct === "furnitures" ? (
+          ) : productForm.product_type === "furniture" ? (
             <div className="productSwitch">
               <h3>Furniture</h3>
               <div>
                 <label htmlFor="height">Height</label>
                 <input
                   name="height"
-                  type="text"
+                  type="number"
                   id="height"
+                  required
+                  value={productForm.height}
                   placeholder="Height"
-                  onChange={(e) => setHeight(e.target.value)}
+                  onChange={handleChange}
                 />
+                {errors.height && <h3>{errors.height}</h3>}
               </div>
               <div>
                 <label htmlFor="width">Width</label>
                 <input
-                  name="size"
-                  type="text"
+                  name="width"
+                  type="number"
                   id="width"
+                  value={productForm.width}
                   placeholder="Width"
-                  onChange={(e) => setWidth(e.target.value)}
+                  required
+                  onChange={handleChange}
                 />
+                {errors.width && <h3>{errors.width}</h3>}
               </div>
               <div>
                 <label htmlFor="length">Length</label>
                 <input
-                  name="size"
+                  name="length"
                   type="text"
                   id="length"
+                  value={productForm.length}
+                  required
                   placeholder="Length"
-                  onChange={(e) => setLength(e.target.value)}
+                  onChange={handleChange}
                 />
+                {errors.length && <h3>{errors.length}</h3>}
               </div>
               <p>
-                <small>This is the furniture description</small>
+                <small>Please Provide Dimension</small>
               </p>
             </div>
           ) : (
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -5,28 +5,47 @@ import { useNavigate } from "react-router-dom";
 const ProductList = () => {
   const navigate = useNavigate();
 
-  const [discs, setDiscs] = useState([]);
-  const [furnitures, setFurnitures] = useState([]);
-  const [books, setBooks] = useState([]);
+  const [products, setProducts] = useState(null);
+  const [checkedBoxes, setCheckedBoxes] = useState([]);
 
   useEffect(() => {
-    fetchProducts();
+    const url =
+      "https://scandiwebphpapi.000webhostapp.com/scandiback/api/getProducts.php";
+    axios.get(url).then((res) => {
+      console.log(res.data);
+      setProducts(res.data.products);
+    });
   }, []);
 
-  const fetchProducts = () => {
-    const url = "http://localhost:8080/scandiback/api/posts/read.php";
-    axios.get(url).then((res) => {
-      setDiscs(res.data.discs);
-      setFurnitures(res.data.furnitures);
-      setBooks(res.data.books);
-    });
+  const toggleCheckBox = (e, product) => {
+    if (e.target.checked) {
+      let arr = checkedBoxes;
+      arr.push(product.sku);
+      setCheckedBoxes(arr);
+    } else {
+      let items = checkedBoxes.splice(checkedBoxes.indexOf(product.sku), 1);
+      setCheckedBoxes(items);
+    }
+    console.log(checkedBoxes);
+  };
+
+  const handleDelete = async () => {
+    let deleteData = new FormData();
+    deleteData.append("skus", checkedBoxes);
+    const url =
+      "https://scandiwebphpapi.000webhostapp.com/scandiback/api/deleteProducts.php";
+    await axios
+      .post(url, deleteData)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+    window.location.reload(true);
   };
 
   return (
     <div className="productListContainer">
-      <div className="row">
+      <div className="row between">
         <h2>Product List</h2>
-        <div>
+        <div className="controlBtns">
           <button
             id="add-button"
             onClick={() => {
@@ -35,68 +54,45 @@ const ProductList = () => {
           >
             ADD
           </button>
-          <button id="delete-product-button">Mass Delete</button>
+          <button id="delete-product-button" onClick={handleDelete}>
+            Mass Delete
+          </button>
         </div>
       </div>
       <hr></hr>
-      <div className="product-list row center">
-        <div className="discs">
-          {discs ? (
-            discs.map((disc) => (
-              <div className="card" key={disc.sku}>
+      <div className="product-list">
+        <div className="products row center">
+          {products ? (
+            products?.map((product) => (
+              <div className="card" key={product.sku}>
                 <div>
-                  <input type="checkbox" className="delete-checkbox" />
+                  <input
+                    type="checkbox"
+                    className="delete-checkbox"
+                    value={product.sku}
+                    checked={checkedBoxes.find(
+                      (item) => item.sku === product.sku
+                    )}
+                    onChange={(e) => toggleCheckBox(e, product)}
+                  />
                 </div>
                 <ul>
-                  <li>SKU: {disc.sku}</li>
-                  <li>Name: {disc.name}</li>
-                  <li>Price: {disc.price}</li>
-                  <li>Size: {disc.size}</li>
+                  <li>SKU: {product.sku}</li>
+                  <li>Name: {product.name}</li>
+                  <li>Price: {product.price}</li>
+                  <li>Price: {product.price}</li>
+                  {product.product_type === "disc" ? (
+                    <li>Size: {product.size}</li>
+                  ) : product.product_type === "book" ? (
+                    <li>Weight: {product.weight}</li>
+                  ) : (
+                    <li>Dimension: {product.dimension}</li>
+                  )}
                 </ul>
               </div>
             ))
           ) : (
-            <React.Fragment>
-              <h3>Server Not Found</h3>
-            </React.Fragment>
-          )}
-        </div>
-        <div className="books">
-          {books ? (
-            books.map((book) => (
-              <div className="card" key={book.sku}>
-                <div>
-                  <input type="checkbox" className="delete-checkbox" />
-                </div>
-                <ul>
-                  <li>SKU: {book.sku}</li>
-                  <li>Name: {book.name}</li>
-                  <li>Price: {book.price}</li>
-                  <li>Weight: {book.weight}</li>
-                </ul>
-              </div>
-            ))
-          ) : (
-            <React.Fragment></React.Fragment>
-          )}
-        </div>
-        <div className="furnitures">
-          {furnitures ? (
-            furnitures.map((furniture) => (
-              <div className="card" key={furniture.sku}>
-                <div>
-                  <input type="checkbox" className="delete-checkbox" />
-                </div>
-                <ul>
-                  <li>SKU: {furniture.sku}</li>
-                  <li>Name: {furniture.name}</li>
-                  <li>Price: {furniture.price}</li>
-                  <li>Dimensions: {furniture.dimensions}</li>
-                </ul>
-              </div>
-            ))
-          ) : (
-            <React.Fragment></React.Fragment>
+            <div>Product Not Found</div>
           )}
         </div>
       </div>
